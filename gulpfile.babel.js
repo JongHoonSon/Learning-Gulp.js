96e94ee306ca7ffa73c3ba15dc96954aa24e019f
JongHoonSon <roka1684@gmail.com>
2022-06-03 20:11:26 +0900

    #1.1 Pug Task part Two
    build 시 기존 파일을 삭제하는 del 사용

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,14 +1,17 @@
 import gulp from "gulp";
-import gulp_pug from "gulp-pug";
+import gpug from "gulp-pug";
+import del from "del";
 
 const routes = {
   pug: {
     src: "src/*.pug",
-    dest: "build/",
+    dest: "build",
   },
 };
 
 export const pug = () =>
-  gulp.src(routes.pug.src).pipe(gulp_pug()).pipe(gulp.dest(routes.pug.dest));
+  gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
 
-export const dev = gulp.series([pug]);
+export const clean = () => del(["build"]);
+
+export const dev = gulp.series([clean, pug]);
