c7ffba94d28be856117ad6252212adeb95dec187
JongHoonSon <roka1684@gmail.com>
2022-06-03 21:13:53 +0900

    파일 변환시, 파일명 앞에 "_" 가 붙은 파일은 사용만 할 뿐 변환시킨 파일을 생성하지 않음

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,8 @@ import del from "del";
 import ws from "gulp-webserver";
 import gimage from "gulp-image";
 
+const gsass = require("gulp-sass")(require("node-sass"));
+
 const routes = {
   pug: {
     watch: "src/**/*.pug",
@@ -14,6 +16,11 @@ const routes = {
     src: "src/img/*",
     dest: "build/img",
   },
+  scss: {
+    watch: "src/scss/**/*.scss",
+    src: "src/scss/style.scss",
+    dest: "build/css",
+  },
 };
 
 const pug = () =>
@@ -21,6 +28,12 @@ const pug = () =>
 
 const img = () =>
   gulp.src(routes.img.src).pipe(gimage()).pipe(gulp.dest(routes.img.dest));
+
+const styles = () =>
+  gulp
+    .src(routes.scss.src)
+    .pipe(gsass().on("error", gsass.logError))
+    .pipe(gulp.dest(routes.scss.dest));
 
 const clean = () => del(["build"]);
 
@@ -30,11 +43,12 @@ const webserver = () =>
 const watch = () => {
   gulp.watch(routes.pug.watch, pug);
   gulp.watch(routes.img.src, img);
+  gulp.watch(routes.scss.src, styles);
 };
 
 const prepare = gulp.series([clean, img]);
 
-const assets = gulp.series([pug]);
+const assets = gulp.series([pug, styles]);
 
 const post = gulp.parallel([webserver, watch]);
 
