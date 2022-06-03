ddc2a2b272ebc3b8b34ecb8f6bcd2b6d40ef29bc
JongHoonSon <roka1684@gmail.com>
2022-06-03 19:57:34 +0900

    #1.0 Pug Task part One
    pug를 html로 변환하는 설정 추가
    
    gulp_pug : pug를 변환하는 플러그인
    src : 변환할 파일의 경로
    dest : 변환된 파일이 위치할 경로
    pipe : 양쪽을 이어주는 역할
    
    dev를 pug task를 실행하는 task로 변경함

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,3 +1,14 @@
 import gulp from "gulp";
+import gulp_pug from "gulp-pug";
 
-export const dev = () => console.log("I will dev");
+const routes = {
+  pug: {
+    src: "src/*.pug",
+    dest: "build/",
+  },
+};
+
+export const pug = () =>
+  gulp.src(routes.pug.src).pipe(gulp_pug().pipe(gulp.dest(routes.pug.dest)));
+
+export const dev = gulp.series([pug]);
